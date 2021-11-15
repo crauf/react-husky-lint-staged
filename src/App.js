f05860a6795337af5a8dd7ebdeedccb8b98f184b
butterfly-coder <butterfly17123@protonmail.com>
2021-11-15 03:32:13 -0500

    update eslint config

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 const App = function () {
     return (
-        <div className='App'>
+        <div className="App">
             <header className='App-header'>
                 <img src={logo} className='App-logo' alt='logo' />
                 <p>
                     Edit <code>src/App.js</code> and save to reload
                 </p>
                 <p>Update esLint</p>
+                <p>remove esLint</p>
                 <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
                     Learn React
                 </a>
